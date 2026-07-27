5ceb33ee332b5ec45878f1a9b9592a0828728cfc
Andreas Lauser <andreas.lauser@mercedes-benz.com>
2026-07-27 12:21:26 +0200

    `VariableGroup`: fix `.from_et()`
    `VariableGroup` derives from `IdentifiableElement`, not `NamedElement`.
    
    Signed-off-by: Andreas Lauser <andreas.lauser@mercedes-benz.com>
    Approved-by: Katja Köhler <katja.koehler@mercedes-benz.com>

--- odxtools/variablegroup.py
+++ odxtools/variablegroup.py
@@ -4,7 +4,7 @@ from dataclasses import dataclass
 from typing import TYPE_CHECKING, runtime_checkable
 from xml.etree import ElementTree
 
-from .element import IdentifiableElement, NamedElement
+from .element import IdentifiableElement
 from .nameditemlist import NamedItemList
 from .odxdoccontext import OdxDocContext
 from .utils import dataclass_fields_asdict
@@ -26,6 +26,6 @@ class VariableGroup(IdentifiableElement):
 
     @staticmethod
     def from_et(et_element: ElementTree.Element, context: OdxDocContext) -> "VariableGroup":
-        kwargs = dataclass_fields_asdict(NamedElement.from_et(et_element, context))
+        kwargs = dataclass_fields_asdict(IdentifiableElement.from_et(et_element, context))
 
         return VariableGroup(**kwargs)
